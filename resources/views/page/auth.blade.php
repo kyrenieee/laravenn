f8231631d1ee64676910bcf7f2ebaa6468e72aa6
ARR2005 <Aldwinrich90@gmail.com>
2026-03-27 22:39:05 +0800

    Added Navbar

---
+++ resources/views/page/auth.blade.php
@@ -0,0 +1,12 @@
+<nav>
+    <h1></h1>
+
+    <div>
+        <ul>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <h1 class=" font-semibold text-1xl" >NETFLEX</h1>
+
+    <div>
+        <ul class="list-none text-1xl" >
+            <li >Movies</li>
+            <li >Cinemas</li>
+            <li >Shop</li>
+        </ul>
+    </div>
+    <div>
+        <h1 class=" font-semibold text-1xl" >SIGN IN</h1>
+        <i class="icon-svg"></i>
+    </div>
+</nav>
